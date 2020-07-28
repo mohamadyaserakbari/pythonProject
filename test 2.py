e244c75991e07a22c647261e7f618a3df4116b2d
mohamadyaserakbari <myaser.akbari@gmail.com>
2020-07-28 16:17:52 +0430

    test2.py con right and insert a thing in it

--- test 2.py
+++ test 2.py
@@ -4,5 +4,6 @@ import mysql.connector
 
 cnx = mysql.connector.connect(user='root', database='learn')
 cursor = cnx.cursor()
-cursor.execute('insert into people values (\'far\',\'f\',38)')
+cursor.execute('insert into people values (\'far\',38,\'f\')')
+cnx.commit()
 cnx.close()
